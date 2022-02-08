d2ea981425760258c569be3c3fa864dd6baecfad
stephankmin <33536922+stephankmin@users.noreply.github.com>
2022-02-08 14:49:45 -0800

    added Compound Governance contracts

--- test/governance-test.ts
+++ test/governance-test.ts
@@ -1,5 +1,5 @@
-const { expect } = require("chai");
-const { ethers } = require("hardhat");
+import { ethers } from "hardhat";
+import { expect } from "chai";
 
 describe("Greeter", function () {
   it("Should return the new greeting once it's changed", async function () {
