2626b2b1e0718f8da56524cc675008eac138756c
stephankmin <33536922+stephankmin@users.noreply.github.com>
2022-02-14 18:03:58 -0800

    added governor address state variable

--- contracts/Document.sol
+++ contracts/Document.sol
@@ -3,10 +3,24 @@ pragma solidity ^0.8.11;
 import "node_modules/@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
 
 contract Document is ERC721Upgradeable, IGovernee {
+    address public governor;
     uint256 public docHash;
     string private baseURI;
 
-    function initialize() public initializer {
+    event NewDocHash(string docHash);
+
+    event NewBaseURI(string baseURI);
+
+    modifier onlyGovernor() {
+        require(
+            msg.sender == governor,
+            "Only the governor contract can call this function"
+        );
+        _;
+    }
+
+    function initialize(address _governor) public initializer {
+        governor = _governor;
         __ERC721_init("livedoc", "DOC");
         _safeMint(address(this), 1);
     }
@@ -21,15 +35,33 @@ contract Document is ERC721Upgradeable, IGovernee {
         return baseURI;
     }
 
-    function setBaseURI(string memory _baseURI) private virtual onlyOwner {
+    function setBaseURI(string memory _baseURI) private virtual onlyGovernor {
         baseURI = _baseURI;
+
+        emit NewBaseURI(_baseURI);
     }
 
-    function setNewDocHash(string memory _docHash) private virtual onlyOwner {
+    function setNewDocHash(uint256 _docHash) internal virtual onlyGovernor {
         docHash = _docHash;
+
+        emit NewDocHash(_docHash);
     }
 
-    function getCurrentDocURI() public virtual returns (string memory) {
-        return super.tokenURI(1);
+    function tokenURI() public view virtual returns (string memory) {
+        string memory baseURI = _baseURI();
+        return
+            bytes(baseURI).length > 0
+                ? string(abi.encodePacked(baseURI, docHash.toString()))
+                : "";
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        virtual
+        override
+        returns (string memory)
+    {
+        revert();
     }
 }
